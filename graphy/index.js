0b7179c1e6216011256701d7c9e7f7d732b6f5a5
amirhossain67 <58366869+amirhossain67@users.noreply.github.com>
2025-06-03 21:49:17 +0400

    Jeferson did something

--- graphy/index.js
+++ graphy/index.js
@@ -32,7 +32,7 @@ function canvasaxis() {
 function getgraphlist() {
     let a = []
     let co = []
-    let c = document.getElementsByClassName("functionpart")
+    let c = document.getElementsByClassName("BROIMANIDIOT")
     for (let x = 0; x < c.length; x++) {
         let k = c.item(x).value
         co.push(c.item(x).getAttribute("graphcolor"))
@@ -42,14 +42,24 @@ function getgraphlist() {
     return [a,co];
 }
 function randomize(min,max) {
-    return min + Math.floor(Math.random()*(max-min))
+    return parseInt(min) + Math.floor(Math.random()*(parseInt(max)-parseInt(min)))
 }
 function graphs(graphlist = getgraphlist()[0], graphcolors = getgraphlist()[1]) {
     graphreset()
     for (let y = 0; y < graphlist.length; y++) {
-        let func = function (x,formula) {return math.evaluate(formula.replaceAll("x","(" + x + ")"))};
+        let func = function (x,formula) {return math.evaluate(SUPERPARSE(formula,x))};
         drawgraph(func,graphlist[y],graphcolors[y])
     }
+}
+function SUPERPARSE(text,z,zacc=0) {
+    text = text.replaceAll("x",`(${(z-zacc)*zoommultiplier})`).replaceAll("t",`(${(z-zacc)*zoommultiplier})`).replaceAll("z",`(${(z-zacc)*zoommultiplier})`).split("{rnd");
+    for (let i = 1; i < text.length; i++) {
+        let kk = text[i].split("}")
+        let rndgen = kk[0].split(",")
+        let rndnum = randomize(math.evaluate(rndgen[0]),math.evaluate(rndgen[1]))
+        kk.shift();
+        text[i] = `(${rndnum})` + kk.join('')
+    } return text.join('')
 }
 function graphsprime() {
     graphreset()
@@ -64,8 +74,23 @@ function graphsprime() {
     if(ztime != 0) {
         z = zacc
         func = (formula1,formula2,zacc,zlim,z,ztime) => {setTimeout(()=>{
-            graph.moveTo(graphmargin[1][0]-math.evaluate(formula1.replaceAll("z","(" + ((z-zacc))*zoommultiplier + ")")),graphmargin[1][1]-math.evaluate(formula2.replaceAll("z","(" + ((z-zacc))*zoommultiplier + ")")))
-            graph.lineTo(graphmargin[1][0]-math.evaluate(formula1.replaceAll("z","(" + ((z))*zoommultiplier + ")")),graphmargin[1][1]-math.evaluate(formula2.replaceAll("z","(" + ((z))*zoommultiplier + ")")))
+            if (document.getElementById("PLOTPOINT").checked) {
+                graph.fillStyle = `rgb(100,100,100)`
+                graph.strokeStyle = `rgb(50,50,50)`
+                graph.arc(graphmargin[1][0]-math.evaluate(SUPERPARSE(formula1,z,zacc)),graphmargin[1][1]-math.evaluate(SUPERPARSE(formula2,z,zacc)),document.getElementById("PLOTSIZE").value/10,0,2*Math.PI)
+                graph.fill()
+                graph.stroke()
+                graph.beginPath()
+                graph.setLineDash([])
+                graph.arc(graphmargin[1][0]-math.evaluate(SUPERPARSE(formula1,z)),graphmargin[1][1]-math.evaluate(SUPERPARSE(formula2,z)),document.getElementById("PLOTSIZE").value/10,0,2*Math.PI)
+                graph.fill()
+                graph.stroke()
+                graph.beginPath()
+                graph.setLineDash([])
+            } else {
+            graph.moveTo(graphmargin[1][0]-math.evaluate(SUPERPARSE(formula1,z,zacc),graphmargin[1][1]-math.evaluate(SUPERPARSE(formula2,z,zacc))))
+            graph.lineTo(graphmargin[1][0]-math.evaluate(SUPERPARSE(formula1,z),graphmargin[1][1]-math.evaluate(SUPERPARSE(formula2,z))))
+        }
             graph.stroke()
             z += zacc;
             if (z < zlim)
@@ -75,21 +100,109 @@ function graphsprime() {
     }
     else {
         for (let z = zacc; z <= zlim; z+=zacc) {
-            graph.moveTo(graphmargin[1][0]-math.evaluate(formula1.replaceAll("z","(" + ((z-zacc))*zoommultiplier + ")")),graphmargin[1][1]-math.evaluate(formula2.replaceAll("z","(" + ((z-zacc))*zoommultiplier + ")")))
-            graph.lineTo(graphmargin[1][0]-math.evaluate(formula1.replaceAll("z","(" + ((z))*zoommultiplier + ")")),graphmargin[1][1]-math.evaluate(formula2.replaceAll("z","(" + ((z))*zoommultiplier + ")")))
-        }
+            if (document.getElementById("PLOTPOINT").checked) {
+                graph.fillStyle = `rgb(100,100,100)`
+                graph.strokeStyle = `rgb(50,50,50)`
+                graph.arc(graphmargin[1][0]-math.evaluate(SUPERPARSE(formula1,z,zacc)),graphmargin[1][1]-math.evaluate(SUPERPARSE(formula2,z,zacc)),document.getElementById("PLOTSIZE").value/10,0,2*Math.PI)
+                graph.fill()
+                graph.stroke()
+                graph.beginPath()
+                graph.setLineDash([])
+                graph.arc(graphmargin[1][0]-math.evaluate(SUPERPARSE(formula1,z)),graphmargin[1][1]-math.evaluate(SUPERPARSE(formula2,z)),document.getElementById("PLOTSIZE").value/10,0,2*Math.PI)
+                graph.fill()
+                graph.stroke()
+                graph.beginPath()
+                graph.setLineDash([])
+            } else {
+            graph.moveTo(graphmargin[1][0]-math.evaluate(SUPERPARSE(formula1,z,zacc),graphmargin[1][1]-math.evaluate(SUPERPARSE(formula2,z,zacc))))
+            graph.lineTo(graphmargin[1][0]-math.evaluate(SUPERPARSE(formula1,z),graphmargin[1][1]-math.evaluate(SUPERPARSE(formula2,z))))
+        }}
     }
     graph.stroke()
 }
-function drawgraph(func,formula,color=`rgb(${randomize(0,200)},${randomize(0,200)},${randomize(0,200)})`) {
+function graphszegon() {
+    graphreset()
+    zlim = parseInt(document.getElementById("alimit").value)
+    formula1 = document.getElementById("anginp1").value
     graph.beginPath()
     graph.setLineDash([])
-    graph.strokeStyle = color
+    zacc = 1/parseInt(document.getElementById("aacc").value)
+    graph.strokeStyle = "rgb(50,0,50)"
+    ztime = parseFloat(document.getElementById("animtime").value)
+    if(ztime != 0) {
+        z = zacc
+        func = (formula1,zacc,zlim,z,ztime) => {setTimeout(()=>{
+            if (document.getElementById("PLOTPOINT").checked) {
+                graph.fillStyle = `rgb(100,100,100)`
+                graph.strokeStyle = `rgb(50,50,50)`
+                graph.arc(graphmargin[1][0]-math.evaluate(SUPERPARSE(formula1,z,zacc))*math.evaluate(`cos(${(z-zacc)*zoommultiplier})`),graphmargin[1][1]-math.evaluate(SUPERPARSE(formula1,z,zacc))*math.evaluate(`sin(${(z-zacc)*zoommultiplier})`),document.getElementById("PLOTSIZE").value/10,0,2*Math.PI)
+                graph.fill()
+                graph.stroke()
+                graph.beginPath()
+                graph.setLineDash([])
+                graph.arc(graphmargin[1][0]-math.evaluate(SUPERPARSE(formula1,z))*math.evaluate(`cos(${(z)*zoommultiplier})`),graphmargin[1][1]-math.evaluate(SUPERPARSE(formula1,z))*math.evaluate(`sin(${(z)*zoommultiplier})`),document.getElementById("PLOTSIZE").value/10,0,2*Math.PI)
+                graph.fill()
+                graph.stroke()
+                graph.beginPath()
+                graph.setLineDash([])
+            } else {
+            graph.moveTo(graphmargin[1][0]-math.evaluate(SUPERPARSE(formula1,z,zacc))*math.evaluate(`cos(${(z-zacc)*zoommultiplier})`),graphmargin[1][1]-math.evaluate(SUPERPARSE(formula1,z,zacc))*math.evaluate(`sin(${(z-zacc)*zoommultiplier})`))
+            graph.lineTo(graphmargin[1][0]-math.evaluate(SUPERPARSE(formula1,z))*math.evaluate(`cos(${(z)*zoommultiplier})`),graphmargin[1][1]-math.evaluate(SUPERPARSE(formula1,z))*math.evaluate(`sin(${(z)*zoommultiplier})`))
+            }
+            graph.stroke()
+            z += zacc;
+            if (z < zlim)
+                func(formula1,zacc,zlim,z)
+        },ztime)}
+        func(formula1,zacc,zlim,z,ztime)
+    }
+    else {
+        for (let z = zacc; z <= zlim; z+=zacc) {
+            if (document.getElementById("PLOTPOINT").checked) {
+                graph.fillStyle = `rgb(100,100,100)`
+                graph.strokeStyle = `rgb(50,50,50)`
+                graph.arc(graphmargin[1][0]-math.evaluate(SUPERPARSE(formula1,z,zacc))*math.evaluate(`cos(${(z-zacc)*zoommultiplier})`),graphmargin[1][1]-math.evaluate(SUPERPARSE(formula1,z,zacc))*math.evaluate(`sin(${(z-zacc)*zoommultiplier})`),document.getElementById("PLOTSIZE").value/10,0,2*Math.PI)
+                graph.fill()
+                graph.stroke()
+                graph.beginPath()
+                graph.setLineDash([])
+                graph.arc(graphmargin[1][0]-math.evaluate(SUPERPARSE(formula1,z))*math.evaluate(`cos(${(z)*zoommultiplier})`),graphmargin[1][1]-math.evaluate(SUPERPARSE(formula1,z))*math.evaluate(`sin(${(z)*zoommultiplier})`),document.getElementById("PLOTSIZE").value/10,0,2*Math.PI)
+                graph.fill()
+                graph.stroke()
+                graph.beginPath()
+                graph.setLineDash([])
+            } else {
+            graph.moveTo(graphmargin[1][0]-math.evaluate(SUPERPARSE(formula1,z,zacc))*math.evaluate(`cos(${(z-zacc)*zoommultiplier})`),graphmargin[1][1]-math.evaluate(SUPERPARSE(formula1,z,zacc))*math.evaluate(`sin(${(z-zacc)*zoommultiplier})`))
+            graph.lineTo(graphmargin[1][0]-math.evaluate(SUPERPARSE(formula1,z))*math.evaluate(`cos(${(z)*zoommultiplier})`),graphmargin[1][1]-math.evaluate(SUPERPARSE(formula1,z))*math.evaluate(`sin(${(z)*zoommultiplier})`))
+        }}
+    }
+    graph.stroke()
+}
+function drawgraph(func,formula,colorZ=`rgb(${randomize(0,200)},${randomize(0,200)},${randomize(0,200)})`) {
+    graph.beginPath()
+    graph.setLineDash([])
+    graph.strokeStyle = colorZ
     for (let x = 0; x < graphmargin[2][0]-1; x++) {
         try {
             cx = (x-graphmargin[1][0])
-            graph.moveTo(x,graphmargin[1][1]-func(zoommultiplier * cx , formula))
-            graph.lineTo(x+1,graphmargin[1][1]-func(zoommultiplier * (cx+1) , formula))
+            if (document.getElementById("PLOTPOINT").checked) {
+                graph.fillStyle = `rgb(100,100,100)`
+                graph.strokeStyle = `rgb(50,50,50)`
+                graph.arc(x,graphmargin[1][1]-func(cx , formula),document.getElementById("PLOTSIZE").value/10,0,2*Math.PI)
+                graph.fill()
+                graph.stroke()
+                graph.beginPath()
+                graph.setLineDash([])
+                graph.arc(x+1,graphmargin[1][1]-func(cx+1 , formula),document.getElementById("PLOTSIZE").value/10,0,2*Math.PI)
+                graph.fill()
+                graph.stroke()
+                graph.beginPath()
+                graph.setLineDash([])
+                graph.strokeStyle = colorZ
+            } else {
+                graph.moveTo(x,graphmargin[1][1]-func(cx , formula))
+                graph.lineTo(x+1,graphmargin[1][1]-func((cx+1) , formula))
+            }
         } catch {
             console.error("Oops! Undefined!")
         }
